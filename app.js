72b198f720247611a4a95d092e4a6b4ac00ae875
Erin Darling <erinkjewell@gmail.com>
2022-04-28 19:47:56 -0400

    Added Axios and API calls

---
+++ app.js
@@ -0,0 +1,8 @@
+function displayCurrentTemperature(response) {
+  console.log(response.data);
+}
+
+let apiKey = "5921d610e62ddd7cd6382a774739ccc4";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayCurrentTemperature);
