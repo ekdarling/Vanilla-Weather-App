a34b4f673ac3f4f314e303eac7998c526e0bff80
ekdarling <104171706+ekdarling@users.noreply.github.com>
2022-04-29 14:29:19 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function displayCurrentTemperature(response) {
-  console.log(response.data);
-}
-
-let apiKey = "5921d610e62ddd7cd6382a774739ccc4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
-
-axios.get(apiUrl).then(displayCurrentTemperature);
